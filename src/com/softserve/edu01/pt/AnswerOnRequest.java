438cd7c6280e13b22492183ab29d7be79a3e3dc6
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-07-22 13:30:16 +0200

    extra practical task commit

---
+++ src/com/softserve/edu01/pt/AnswerOnRequest.java
@@ -0,0 +1,12 @@
+package src.com.softserve.edu01.pt;
+
+import java.util.Scanner;
+
+public class AnswerOnRequest {
+    public static void main(String[] args) {
+        System.out.println("How are you?");
+        Scanner scanner = new Scanner(System.in);
+        String answer  = scanner.nextLine();
+        System.out.println("You are " + answer.toLowerCase());
+    }
+}
