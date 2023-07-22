f13388da2e7013db6671d29534dc0c0d155e4ae4
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-07-22 13:04:19 +0200

    second practical task commit

---
+++ src/com/softserve/edu01/pt/AvarageNumber.java
@@ -0,0 +1,20 @@
+package src.com.softserve.edu01.pt;
+
+import java.util.Scanner;
+
+public class AvarageNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Write numbers to get avarage:");
+        double[] number = new double[3];
+        double sum = 0;
+        for(int i = 0; i <= 2; i++){
+            number[i] = scanner.nextDouble();
+            sum += number[i];
+        }
+        double result = sum / 3;
+
+        System.out.printf("Your avarage is: %.2f", result);
+    }
+}
