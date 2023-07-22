581c4763f4aa6f364539cb2ab56cf54e47acdbd5
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-07-22 12:33:59 +0200

    first practical task commit

---
+++ src/com/softserve/edu01/pt/ReverseString.java
@@ -0,0 +1,19 @@
+package src.com.softserve.edu01.pt;
+
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write your first string: ");
+        String srt1 = scanner.nextLine();
+        System.out.print("Write your second string: ");
+        String srt2 = scanner.nextLine();
+        System.out.print("Write your third string: ");
+        String srt3 = scanner.nextLine();
+
+        System.out.println(srt3);
+        System.out.println(srt2);
+        System.out.println(srt1);
+    }
+}
