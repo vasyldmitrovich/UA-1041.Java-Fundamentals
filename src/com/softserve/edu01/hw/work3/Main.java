6d92dc33fd67c0a8e48beaabad83646238c5fdc6
Mandarinka4 <maryna.osypova.oi.2022@lpnu.ua>
2023-07-27 13:56:49 +0300

    my first commit

--- src/com/softserve/edu01/hw/work3/Main.java
+++ src/com/softserve/edu01/hw/work3/Main.java
@@ -1,7 +1,37 @@
 package com.softserve.edu01.hw.work3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+    double c1, c2, c3;
+    double t1, t2, t3;
+    double v1, v2, v3; // вартість
+        double cost; // загальна вартість
 
+        System.out.println("Введіть вартість хвилини першого дзвінка: ");
+        Scanner scanner = new Scanner(System.in);
+        c1 = scanner.nextDouble();
+        System.out.println("Введіть вартість хвилини другого дзвінка: ");
+        c2 = scanner.nextDouble();
+        System.out.println("Введіть вартість хвилини третього дзвінка: ");
+        c3 = scanner.nextDouble();
+
+        System.out.println("Введіть тривалість першого дзвінка: ");
+        t1 = scanner.nextDouble();
+        System.out.println("Введіть тривалість другого дзвінка: ");
+        t2 = scanner.nextDouble();
+        System.out.println("Введіть тривалість третього дзвінка: ");
+        t3 = scanner.nextDouble();
+
+        v1 = c1 * t1;
+        v2 = c2 * t2;
+        v3 = c3 * t3;
+        cost = v1 + v2 + v3;
+
+        System.out.println("Вартість першого дзвінка: " + v1);
+        System.out.println("Вартість другого дзвінка: " + v2);
+        System.out.println("Вартість третього дзвінка: " + v2);
+        System.out.println("Загальна варість: " + cost);
     }
 }
