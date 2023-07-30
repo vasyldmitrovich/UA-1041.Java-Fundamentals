c863de70f88173a5fa130759b8628c5f30321a1d
DSkorbach <DSkorbach@i.ua>
2023-07-30 18:25:38 +0300

    hw3

--- src/com/softserve/edu01/hw/Main.java
+++ src/com/softserve/edu01/hw/Main.java
@@ -5,10 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         var Scanner = new Scanner(System.in);
-        System.out.print("Enter r = ");
-        int r = Scanner.nextInt();
-        Double pi = Math.PI;
-        System.out.println("pi * (r*r) = " + pi * (r*r));
-        System.out.println("2 * pi * r = " + 2 * pi * r);
+        System.out.print("Enter c1 = ");
+        int c1 = Scanner.nextInt();
+        System.out.print("Enter c2 = ");
+        int c2 = Scanner.nextInt();
+        System.out.print("Enter c3 = ");
+        int c3 = Scanner.nextInt();
+        System.out.print("Enter t1 = ");
+        int t1 = Scanner.nextInt();
+        System.out.print("Enter t2 = ");
+        int t2 = Scanner.nextInt();
+        System.out.print("Enter t3 = ");
+        int t3 = Scanner.nextInt();
+        System.out.println("c1 * t1 = " + (c1 * t1));
+        System.out.println("c2 * t2 = " + (c2 * t2));
+        System.out.println("c3 * t3 = " + (c3 * t3));
+
     }
 }
