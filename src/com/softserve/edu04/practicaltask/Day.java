22ad64cda425525921fc9306e0b5cc9d62f7797e
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-07-30 12:43:35 +0200

    Practical task edu04: Added Enum Day.java

---
+++ src/com/softserve/edu04/practicaltask/Day.java
@@ -0,0 +1,27 @@
+package src.com.softserve.edu04.practicaltask;
+
+public enum Day {
+    MONDAY("Monday", "Понеділок"),
+    TUESDAY("Tuesday", "Вівторок"),
+    WEDNESDAY("Wednesday", "Середа"),
+    THURSDAY("Thursday", "Четверг"),
+    FRIDAY("Friday", "Пʼятниця"),
+    SATURDAY("Saturday", "Субота"),
+    SUNDAY("Sunday", "Неділя");
+
+    private final String en;
+    private final String ua;
+
+    Day(String en, String ua) {
+        this.en = en;
+        this.ua = ua;
+    }
+
+    public String getEn() {
+        return en;
+    }
+
+    public String getUa() {
+        return ua;
+    }
+}
