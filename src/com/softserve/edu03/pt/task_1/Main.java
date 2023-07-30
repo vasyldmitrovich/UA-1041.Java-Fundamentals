d1284a7cc79df817b01665de597f3cded27a8fe3
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-07-30 22:37:34 +0300

    practical task 1 less3

--- src/com/softserve/edu03/pt/task_1/Main.java
+++ src/com/softserve/edu03/pt/task_1/Main.java
@@ -1,17 +1,28 @@
 package com.softserve.edu03.pt.task_1;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+int a=getNumber("input first  number:");
+        int b=getNumber("input second  number:");
+        int total=getTotal(a,b);
+        double average=getAverage(a,b);
+        System.out.format("The sum of %d and %d is %d\n",a,b,total);
+        System.out.format("The average of %d and %d is %.1f",a,b,average);
     }
 
-    public int getTotal(int a, int b) {
+    public static int getTotal(int a, int b) {
         return a + b;
     }
-    public double getAverage (int a, int b) {
-        return (a + b)*0.5;
-    }
-    public  static void getNumber (String prompt){
 
+    public static double getAverage(int a, int b) {
+        return (a + b) * 0.5;
+    }
+
+    public static int getNumber(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(prompt);
+        return sc.nextInt();
     }
 }
