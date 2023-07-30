032015e6fbf7609095289d57f6c067703cade1b8
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-07-30 12:44:30 +0200

    Practical task edu04: Added class NameOfContinent.java

---
+++ src/com/softserve/edu04/practicaltask/Main.java
@@ -0,0 +1,93 @@
+package src.com.softserve.edu04.practicaltask;
+
+import java.util.Scanner;
+
+
+public class Main {
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numOfTask;
+
+        do {
+            printMenu();
+            System.out.print("\nPlease input the number of the task: ");
+            numOfTask = SCANNER.nextInt();
+            SCANNER.nextLine();
+            System.out.println("Performing task number " + numOfTask);
+
+            switch (numOfTask) {
+                case 1 -> performFirstTask();
+                case 2 -> performSecondTask();
+                case 3 -> performThirdTask();
+                case 4 -> performFourthTask();
+                case 5 -> performFivesTask();
+                case 0 -> System.out.println("Exiting............");
+                default -> System.out.println("Invalid number of the task. Please select the correct one");
+            }
+            printLine();
+
+        } while (numOfTask != 0);
+
+    }
+
+    private static void performFirstTask() {
+        //First practical task "Find out of the resul how many of numbers are odd"
+
+        OddOrEven obj = new OddOrEven();
+        OddOrEven.promptOddOrEven(obj);
+        obj.isOdd(obj);
+        obj.promptNumOfOdd(obj);
+        printLine();
+        System.out.print("\nTask 1 completed!" +
+                "Press ENTER to go back to the menu");
+        SCANNER.nextLine();
+    }
+
+    private static void performSecondTask() {
+        //Second practical task "Print name of the day by his number"
+
+        DayOfTheWeek day = new DayOfTheWeek();
+        DayOfTheWeek.promptDayName(day);
+        day.nameOfTheDay(day);
+        printLine();
+        System.out.print("\nTask 2 completed!" +
+                "Press ENTER to go back to the menu");
+        SCANNER.nextLine();
+    }
+
+    private static void performThirdTask() {
+        //Third practical task "Output the name of the continent by name of country"
+        NameOfContinent name = new NameOfContinent();
+        NameOfContinent.promptContName(name);
+        printLine();
+        System.out.print("\nTask 3 completed!" +
+                "Press ENTER to go back to the menu");
+        SCANNER.nextLine();
+    }
+
+    private static void performFourthTask() {
+
+    }
+
+    private static void performFivesTask() {
+    }
+
+    public static void printLine() {
+        System.out.println("");
+        int count = 40;
+        for (int i = 0; i < count; i++) {
+            System.out.print("-_-_");
+        }
+    }
+
+    private static void printMenu() {
+        System.out.print("\nChose the task which you want to see :)" +
+                "\n\t1 - First practical task" +
+                "\n\t2 - Second practical task" +
+                "\n\t3 - Third practical task" +
+                "\n\t4 - Fourth practical task" +
+                "\n\t5 - Fives practical task" +
+                "\n\t0 - Exit");
+    }
+}
---
+++ src/com/softserve/edu04/practicaltask/NameOfContinent.java
@@ -0,0 +1,77 @@
+package src.com.softserve.edu04.practicaltask;
+
+import static src.com.softserve.edu03.homework1.Main.SCANNER;
+import static src.com.softserve.edu04.practicaltask.Continent.*;
+
+public class NameOfContinent {
+    private String nameCont;
+
+    public NameOfContinent() {
+
+    }
+
+    public NameOfContinent(String nameCont) {
+        this.nameCont = nameCont;
+    }
+
+    public static void promptContName(NameOfContinent name) {
+        System.out.print("Input the name of the country number: ");
+        String countryName = SCANNER.nextLine();
+        name.setNameCont(countryName);
+        System.out.print("Name of continent is: ");
+        name.getContinentName(name);
+    }
+
+    public String getNameCont() {
+        return nameCont;
+    }
+
+    public void setNameCont(String nameCont) {
+        this.nameCont = nameCont;
+    }
+
+    public void getContinentName(NameOfContinent name) {
+        switch (getNameCont().toLowerCase()) {
+            case "algeria", "angola", "benin", "botswana", "burkina faso", "burundi", "cabo verde", "cameroon",
+                    "central african republic", "chad", "comoros", "congo", "democratic republic of the congo",
+                    "djibouti", "egypt", "equatorial guinea", "eritrea", "eswatini", "ethiopia", "gabon", "gambia",
+                    "ghana", "guinea", "guinea-bissau", "ivory coast", "kenya", "lesotho", "liberia", "libya",
+                    "madagascar", "malawi", "mali", "mauritania", "mauritius", "morocco", "mozambique", "namibia",
+                    "niger", "nigeria", "rwanda", "sao tome and principe", "senegal", "seychelles", "sierra leone",
+                    "somalia", "south africa", "south sudan", "sudan", "tanzania", "togo", "tunisia", "uganda",
+                    "zambia", "zimbabwe" -> System.out.println(AFRICA.getName());
+
+            case "antarctica" -> System.out.println(ANTARCTICA.getName());
+
+            case "afghanistan", "armenia", "azerbaijan", "bahrain", "bangladesh", "bhutan", "brunei", "cambodia",
+                    "china", "cyprus", "georgia", "india", "indonesia", "iran", "iraq", "israel", "japan", "jordan",
+                    "kazakhstan", "kuwait", "kyrgyzstan", "laos", "lebanon", "malaysia", "maldives", "mongolia",
+                    "myanmar", "nepal", "north korea", "oman", "pakistan", "palestine", "philippines", "qatar",
+                    "saudi arabia", "singapore", "south korea", "sri lanka", "syria", "tajikistan", "thailand",
+                    "timor-leste", "turkey", "turkmenistan", "united arab emirates", "uzbekistan", "vietnam", "yemen" ->
+                    System.out.println(ASIA.getName());
+
+            case "albania", "andorra", "austria", "belarus", "belgium", "bosnia and herzegovina", "bulgaria", "croatia",
+                    "czech republic", "denmark", "estonia", "finland", "france", "germany", "greece", "hungary",
+                    "iceland", "ireland", "italy", "kosovo", "latvia", "liechtenstein", "lithuania", "luxembourg",
+                    "malta", "moldova", "monaco", "montenegro", "netherlands", "north macedonia", "norway", "poland",
+                    "portugal", "romania", "russia", "san marino", "serbia", "slovakia", "slovenia", "spain", "sweden",
+                    "switzerland", "ukraine", "united kingdom", "vatican city" -> System.out.println(EUROPE.getName());
+
+            case "antigua and barbuda", "bahamas", "barbados", "belize", "canada", "costa rica", "cuba", "dominica",
+                    "dominican republic", "el salvador", "grenada", "guatemala", "haiti", "honduras", "jamaica",
+                    "mexico", "nicaragua", "panama", "saint kitts and nevis", "saint lucia",
+                    "saint vincent and the grenadines", "trinidad and tobago", "united states of america" ->
+                    System.out.println(NORTH_AMERICA.getName());
+
+            case "argentina", "bolivia", "brazil", "chile", "colombia", "ecuador", "guyana", "paraguay", "peru",
+                    "suriname", "uruguay", "venezuela" -> System.out.println(SOUTH_AMERICA.getName());
+
+            case "australia", "fiji", "kiribati", "marshall islands", "micronesia", "nauru", "new zealand", "palau",
+                    "papua new guinea", "samoa", "solomon islands", "tonga", "tuvalu", "vanuatu" ->
+                    System.out.println(AUSTRALIA.getName());
+
+            default -> throw new IllegalArgumentException("Please make sure that you correct provide the name of the country");
+        }
+    }
+}
