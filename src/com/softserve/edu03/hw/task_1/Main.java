76d3bbae18a2f613353979bd80de132fffb4e037
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-08-01 00:04:27 +0300

    homework task 1 less3

---
+++ src/com/softserve/edu03/hw/task_1/Main.java
@@ -0,0 +1,25 @@
+package com.softserve.edu03.hw.task_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int side1 = getSide("Input side 1:");
+        int side2 = getSide("Input side 2:");
+        int side3 = getSide("Input side 3:");
+        System.out.printf("The area of the triangle is %.2f", getArea(side1, side2, side3));
+
+    }
+
+    public static int getSide(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(prompt);
+        return sc.nextInt();
+    }
+
+    public static double getArea(int a, int b, int c) {
+        double p = (a + b + c) * 0.5;
+        double area = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        return area;
+    }
+}
