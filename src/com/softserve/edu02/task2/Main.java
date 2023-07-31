ae13adde01986cbd08464c15291c0989b07be323
alyonaMol <alen4hukm@gmail.com>
2023-07-31 09:26:06 +0300

    new commit5

---
+++ src/com/softserve/edu02/task2/Main.java
@@ -0,0 +1,41 @@
+package com.softserve.edu02.task2;
+
+import java.util.Scanner;
+
+public class Main {
+        public static void main(String[] args) {
+                var scanner = new Scanner(System.in);
+
+
+                System.out.print("Input first number: ");
+                int A = scanner.nextInt();
+                System.out.print("Input second number: ");
+                int B = scanner.nextInt();
+                System.out.print("Input third number: ");
+                int C = scanner.nextInt();
+                int min = Math.min(A, B);
+
+
+                if (min > C) {
+                    min = C;
+        }
+                System.out.print("Min number is = " + min);
+
+
+}
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
