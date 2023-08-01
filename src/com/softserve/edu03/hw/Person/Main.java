8de3eaf8af8a285583b7df771914b86dde414ce7
Holotsvan Ivan <iholotsvan@gmail.com>
2023-08-01 19:25:25 +0300

    Delete unnecessary code

--- src/com/softserve/edu03/hw/Person/Main.java
+++ src/com/softserve/edu03/hw/Person/Main.java
@@ -6,22 +6,7 @@ public class Main {
     public static void main(String[] args) {
         Person p1 = new Person();
         p1.input();
-        //if the following code is not needed, delete it
-       /* Person p2 = new Person();
-        p2.input();
-        Person p3 = new Person();
-        p3.input();
-        Person p4 = new Person();
-        p4.input();
-        Person p5 = new Person();
-        p5.input();
-        */
-
-
         p1.output();
-        /*p2.output();
-        p3.output();
-        p4.output();
-        p5.output();*/
+
     }
 }
