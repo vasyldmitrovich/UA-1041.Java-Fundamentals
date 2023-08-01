4fbc175a30d5b8103c55c57c3c80b61077036412
alyonaMol <alen4hukm@gmail.com>
2023-08-01 13:38:01 +0300

    correct

--- src/com/softserve/edu02/task1/Main.java
+++ src/com/softserve/edu02/task1/Main.java
@@ -6,13 +6,16 @@ public class Main {
     public static void main(String[] args){
         var scanner = new Scanner(System.in);
         System.out.print("Input site 1: ");
-        double A = scanner.nextDouble();
+        double a = scanner.nextDouble();
+
         System.out.print("Input site 2: ");
-        double B = scanner.nextDouble();
+        double b = scanner.nextDouble();
+
         System.out.print("Input site 3: ");
-        double C = scanner.nextDouble();
-        double P = ((A +B +C)/2);
-        double Sqr = Math.sqrt(P* (P-A)*(P-B)*(P-C));
+        double c = scanner.nextDouble();
+
+        double P = ((a +b +c)/2);
+        double Sqr = Math.sqrt(P* (P-a)*(P-b)*(P-c));
         System.out.printf("%.2f", Sqr);
 
 
