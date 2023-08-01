62029a0f95bf955a9242a38eca6ab6ca1e163b3a
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-08-01 20:21:28 +0300

    homework task 1  less4

---
+++ src/com/softserve/edu04/hw/task_1/Main.java
@@ -0,0 +1,26 @@
+package com.softserve.edu04.hw.task_1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+        System.out.println("Input three numbers");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = getNumber("input " + (i + 1) + "st number:");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= -5 && arr[i] <= 5) {
+                System.out.println("number: " + arr[i] + " is belong to the range [-5, 5].");
+            } else {
+                System.out.println("number: " + arr[i] + " isn't belong to the range [-5, 5].");
+            }
+        }
+    }
+
+    public static int getNumber(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(prompt);
+        return sc.nextInt();
+    }
+}
