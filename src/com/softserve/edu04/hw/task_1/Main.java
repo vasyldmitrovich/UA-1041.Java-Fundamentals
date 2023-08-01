767596c3299e25efc25e82a68c9c2df657c464f6
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-08-01 20:42:11 +0300

    homework task 1  less4

--- src/com/softserve/edu04/hw/task_1/Main.java
+++ src/com/softserve/edu04/hw/task_1/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = new int[3];
-        System.out.println("Input three numbers");
+        float[] arr = new float[3];
+        System.out.println("Input three float numbers(use ',')");
         for (int i = 0; i < arr.length; i++) {
             arr[i] = getNumber("input " + (i + 1) + "st number:");
         }
         areBelongRange(arr);
     }
 
-    private static void areBelongRange(int[] arr) {
+    private static void areBelongRange(float[] arr) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= -5 && arr[i] <= 5) {
+            if (arr[i] >= -5.0 && arr[i] <= 5.0) {
                 System.out.println("number: " + arr[i] + " is belong to the range [-5, 5].");
             } else {
                 System.out.println("number: " + arr[i] + " isn't belong to the range [-5, 5].");
@@ -23,9 +23,9 @@ public class Main {
     }
 
 
-    public static int getNumber(String prompt) {
+    public static float getNumber(String prompt) {
         Scanner sc = new Scanner(System.in);
         System.out.print(prompt);
-        return sc.nextInt();
+        return sc.nextFloat();
     }
 }
