5d440d8846c8873b43b9a23d4060386f261183ad
Holotsvan Ivan <iholotsvan@gmail.com>
2023-08-01 19:27:51 +0300

    reformat code and give more understandable names to the variables in method parameter

--- src/com/softserve/edu03/hw/Triangle.java
+++ src/com/softserve/edu03/hw/Triangle.java
@@ -7,11 +7,12 @@ public class Triangle {
         int sideA = getSide("Input Side 1:");
         int sideB = getSide("Input Side 2:");
         int sideC = getSide("Input Side 3:");
-        double area = calcArea(sideA,sideB,sideC);
-        System.out.format("The are of the triangle is %.2f units^2",area);
+        double area = calcArea(sideA, sideB, sideC);
+        System.out.format("The are of the triangle is %.2f units^2", area);
     }
-    static int getSide(String prompt){
-        int side = 0 ;
+
+    static int getSide(String prompt) {
+        int side = 0;
         Scanner sc = new Scanner(System.in);
 
         do {
@@ -26,21 +27,22 @@ public class Triangle {
             if (side <= 0) {
                 System.out.println("Invalid input. Please enter a positive number.");
             }
-        }while (side<=0);
+        } while (side <= 0);
 
         return side;
     }
-    //please use reformat code and give more understandable names to the variables in method parameter
-    static double calcArea(int a , int b, int c){
-        double s = (double) ((a+b+c)/2);
+
+
+    static double calcArea(int sideA, int sideB, int sideC) {
+        double s = (double) ((sideA + sideB + sideC) / 2);
 
         /*Side of a triangle cannot be greater than its semiperimeter.
         It contradicts a fundamental property of triangles known as the "Triangle Inequality."*/
-        if(s-a<=0||s-b<=0||s-c<=0){
+        if (s - sideA <= 0 || s - sideB <= 0 || s - sideC <= 0) {
             System.out.println("One of the side is greater than sum of others\nThis is impossible, so try another sides next time.");
             return -1;
         }
-        return Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        return Math.sqrt(s * (s - sideA) * (s - sideB) * (s - sideC));
 
     }
 }
