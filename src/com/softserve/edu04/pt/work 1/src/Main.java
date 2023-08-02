4ea2b8749f614862fb46f916de4323b1cdde4ada
Mandarinka4 <maryna.osypova.oi.2022@lpnu.ua>
2023-08-02 22:28:45 +0300

    homework #4

---
+++ src/com/softserve/edu04/pt/work 1/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+    static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+        int num3;
+
+        System.out.println("Input first number: ");
+        num1 = SCANNER.nextInt();
+        System.out.println("Input second number: ");
+        num2 = SCANNER.nextInt();
+        System.out.println("Input third number: ");
+        num3 = SCANNER.nextInt();
+
+        if (num1 % 2 == 1) {
+            System.out.println("First number is odd");
+        }
+        if (num2 % 2 == 1) {
+            System.out.println("Second number is odd");
+        }
+        if (num3 % 2 == 1) {
+            System.out.println("Third number is odd");
+        }
+    }
+}
