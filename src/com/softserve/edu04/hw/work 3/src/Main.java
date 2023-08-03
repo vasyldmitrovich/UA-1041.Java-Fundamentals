5639e7f2885920d695d9cf6a37e310e2e808eebd
Mandarinka4 <maryna.osypova.oi.2022@lpnu.ua>
2023-08-03 15:20:27 +0300

    homework #4

---
+++ src/com/softserve/edu04/hw/work 3/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    enum HTTPError {
+        BAD_REQUEST, UNAUTHORIZED, PAYMENT_REQUIRED, FORBIDDEN, NOT_FOUND
+    }
+    static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Input a number representing an HTTP error (such as 400, 401, 402, etc.): ");
+        int number = SCANNER.nextInt();
+
+        HTTPError error = getError(number);
+
+        System.out.println("The name of error #" + number + " is: " + error);
+    }
+    public static HTTPError getError(int number) {
+        switch (number) {
+            case 400:
+                return HTTPError.BAD_REQUEST;
+            case 401:
+                return HTTPError.UNAUTHORIZED;
+            case 402:
+                return HTTPError.PAYMENT_REQUIRED;
+            case 403:
+                return HTTPError.FORBIDDEN;
+            case 404:
+                return HTTPError.NOT_FOUND;
+        }
+        return null;
+    }
+}
