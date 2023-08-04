cc6ba0474f8c009dfc1a793febc998aa028e5af5
Anastasia Rusanova <a.i.rusanova@i.ua>
2023-08-04 19:00:22 +0300

    task 1 hw1

---
+++ src/com/softserve/edu01/hw/task1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Input the radius:");
+        Scanner sc = new Scanner(System.in);
+        double radius = Double.parseDouble(sc.nextLine());
+        double pi = Math.PI;
+        double perimeter = 2 * pi * radius;
+        double area = pi * radius * radius;
+        System.out.format("The perimeter of flower bed: %.2f\n", perimeter);
+        System.out.format("The area of flower bed: %.2f", area);
+    }
+}
