f6988b3ccab4b8a100f3e28082b241a00a7e455b
Vasylyshyn Dmytro <dmitrovasilisin24@gmail.com>
2023-08-04 16:19:11 +0300

    First commit

---
+++ src/com/softserve/edu03/task3/App.java
@@ -0,0 +1,25 @@
+package com.softserve.edu03.task3;
+public class App {
+    public static void main(String[] args) {
+
+
+        Person people1=new Person();
+        Person people2=new Person();
+        Person people3=new Person();
+        Person people4=new Person();
+        Person people5=new Person();
+        people1.input();
+        people2.input();
+        people3.input();
+        people4.input();
+        people5.input();
+
+        //please add spaces between different peoples
+        people1.getAge();
+        people1.output();
+        people2.output();
+        people3.output();
+        people4.output();
+        people5.output();
+    }
+}
