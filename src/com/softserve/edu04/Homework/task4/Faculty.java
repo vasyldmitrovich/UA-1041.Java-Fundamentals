6bad1af9354973e2ba2a1c2af14faa2f6b4647d5
alyonaMol <alen4hukm@gmail.com>
2023-08-05 09:28:35 +0300

    Homework4

---
+++ src/com/softserve/edu04/Homework/task4/Faculty.java
@@ -0,0 +1,43 @@
+package com.softserve.edu04.Homework.task4;
+
+import java.util.Scanner;
+
+public class Faculty {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+        System.out.print("Input number of students for winter: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Input number of students for spring: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Input number of students for summer: ");
+        int num3 = scanner.nextInt();
+
+        System.out.print("Input number of students for autumn: ");
+        int num4 = scanner.nextInt();
+
+       Season season1 = Season.AUTUMN;
+       Season season2 = Season.SPRING;
+       Season season3 = Season.SUMMER;
+       Season season4 = Season.WINTER;
+        System.out.println("Number of student in\t" + season1 + " is\t "+ num1);
+        System.out.println("Number of student in\t" + season2 +  " is\t " + num2);
+        System.out.println("Number of student in\t" + season3 + " is\t " + num3);
+        System.out.println("Number of student in\t" + season4 + " is\t " + num4);
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
---
+++ src/com/softserve/edu04/Homework/task4/Seasson.java
@@ -0,0 +1,9 @@
+package com.softserve.edu04.Homework.task4;
+
+enum Season {
+    WINTER, SPRING, SUMMER, AUTUMN
+
+}
+
+
+
