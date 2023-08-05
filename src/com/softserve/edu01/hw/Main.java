56fc17a124edd67a96086a6e91302630945a4f33
Vasyldmitrovich <vasyl.dmitrovich@gmail.com>
2023-08-06 00:11:09 +0300

    I added some comments, and you could make changes in your code using my advices

--- src/com/softserve/edu01/hw/Main.java
+++ src/com/softserve/edu01/hw/Main.java
@@ -7,16 +7,22 @@ public class Main {
         var Scanner = new Scanner(System.in);
         System.out.print("Enter c1 = ");
         int c1 = Scanner.nextInt();
+
         System.out.print("Enter c2 = ");
         int c2 = Scanner.nextInt();
+
         System.out.print("Enter c3 = ");
         int c3 = Scanner.nextInt();
+
         System.out.print("Enter t1 = ");
         int t1 = Scanner.nextInt();
+
         System.out.print("Enter t2 = ");
         int t2 = Scanner.nextInt();
+
         System.out.print("Enter t3 = ");
         int t3 = Scanner.nextInt();
+
         System.out.println("c1 * t1 = " + (c1 * t1));
         System.out.println("c2 * t2 = " + (c2 * t2));
         System.out.println("c3 * t3 = " + (c3 * t3));
