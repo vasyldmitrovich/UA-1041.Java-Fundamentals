84875a9248c71af63c37bb115264837669a53053
Hlib4yk <dmytro.z1409@gmail.com>
2023-08-06 16:48:16 +0300

    Hw4

--- src/com/softserve/edu01/hw/com.zubyk_dmytro.homework/edu5/task2.java
+++ src/com/softserve/edu01/hw/com.zubyk_dmytro.homework/edu5/task2.java
@@ -4,4 +4,26 @@ import java.util.Arrays;
 
 
 public class task2 {
-}
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char choice;
+
+        do {
+            System.out.print("Enter the first number: ");
+            int num1 = scanner.nextInt();
+
+            System.out.print("Enter the second number: ");
+            int num2 = scanner.nextInt();
+
+            int sum = num1 + num2;
+            System.out.println("Sum: " + sum);
+
+            System.out.print("Do you want to perform the operation again? (Y/N): ");
+            choice = scanner.next().charAt(0);
+        } while (choice == 'Y' || choice == 'y');
+
+        System.out.println("Goodbye!");
+        scanner.close();
+    }
+    }
+
