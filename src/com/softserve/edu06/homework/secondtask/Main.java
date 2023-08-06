5ff2e02e41c946024e48aa0a8ce42d2afef37bcb
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-08-06 16:39:26 +0200

    edu06/Homework task #2 added class Main.java

---
+++ src/com/softserve/edu06/homework/secondtask/Main.java
@@ -0,0 +1,13 @@
+package com.softserve.edu06.homework.secondtask;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee[] employee = new Employee[]{
+                new Employee("John", 25, 1500.50),
+                new Developer("Bob", 30, "Senior Java Developer", 6000.2)
+        };
+        for(var staff : employee){
+            System.out.println(staff.report());
+        }
+    }
+}
