b5b668a8ba4c61eb543dba9d1c18483b854575c1
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-08-06 13:33:25 +0200

    edu06/Practical task added class Sedan.java

---
+++ src/com/softserve/edu06/practicaltasts/Sedan.java
@@ -0,0 +1,25 @@
+package com.softserve.edu06.practicaltasts;
+
+public class Sedan extends Car {
+    public Sedan() {
+    }
+
+    public Sedan(String model, int maxSpeed, int yearOfProduction) {
+        super(model, maxSpeed, yearOfProduction);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+    }
+
+    @Override
+    public void stop() {
+        super.stop();
+    }
+}
