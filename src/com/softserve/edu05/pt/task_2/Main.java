c427bc8588c445f797ef4cb5167dc52eb291cdf8
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-08-06 21:34:31 +0300

    practical task 2  less5

---
+++ src/com/softserve/edu05/pt/task_2/Main.java
@@ -0,0 +1,37 @@
+package com.softserve.edu05.pt.task_2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int number;
+        for (; ; ) {
+            number = getNumber("enter a positive integer:");
+            if (number > 0) {
+                break;
+            }
+            System.out.println("Wrong number, try again");
+        }
+        if (isPrimeNumber(number)) {
+            System.out.println("number - " + number + " is a prime number");
+        } else {
+            System.out.println("number - " + number + " is not prime number");
+        }
+    }
+
+    private static boolean isPrimeNumber(int number) {
+
+        for (int i = 2; i <= number - 1; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int getNumber(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(prompt);
+        return sc.nextInt();
+    }
+}
