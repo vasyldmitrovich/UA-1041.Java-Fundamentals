43cef836a49308170aeaafe131af369f98111c6b
Yurii Lukirin <yuralukirin@gmail.com>
2023-08-06 18:21:56 +0200

    implemented task #1

---
+++ src/com/softserve/edu04/hw/Hw_task1.java
@@ -0,0 +1,21 @@
+package com.softserve.edu04.hw;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Hw_task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        float [] listOfNum = new float[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Give me a number: ");
+            float myNum = scanner.nextInt();
+            listOfNum[i] = myNum;
+        }
+        for (float num: listOfNum) {
+            if((num < (-5.0)) || (num > 5.0)) {
+                System.out.println("This number is not in a range: " + num);
+            }
+        }
+    }
+}
