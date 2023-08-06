3e90a27678fff0c7ebd82d01a00fc3c006e60f21
Vasylyshyn Dmytro <dmitrovasilisin24@gmail.com>
2023-08-06 18:48:48 +0300

    First commit

--- src/com/softserve/edu05/pt/Task1.java
+++ src/com/softserve/edu05/pt/Task1.java
@@ -9,9 +9,9 @@ public class Task1 {
         var scanner = new Scanner(System.in);
         String [] ARRAY={"cdsdcs","scscscsd","erfefefef","pojopokpojpo","qweqeqe"};
         int[] arayint= {9, 7, 6, 4, 1};
-        int s=0;
-        boolean d=false;
-        int k=0;
+        int summ=0;
+        boolean flag=false;
+        int counter=0;
         Arrays.sort(ARRAY);
         /**
          * this loop sort the array of string
@@ -23,20 +23,20 @@ public class Task1 {
          * this loop sums all elements of the array
          */
         for (int i = 0; i < 4; i++) {
-            s+=arayint[i];
+            summ+=arayint[i];
         }
-        System.out.println("the average value of 5 array elements: "+(s/4));
+        System.out.println("the average value of 5 array elements: "+(summ/4));
         scanner.nextLine();
         System.out.println("input a number ");
         var users=scanner.nextLine();
 
         for (int i = 0; i < ARRAY.length; i++) {
             if(users.equals(ARRAY[i])){
-                d=true;
-                k++;
+                flag=true;
+                counter++;
             }
         }
 
-        System.out.println("your value: "+(users)+"the same value in the array: "+(d)+"it value repeated: "+(k));
+        System.out.println("your value: "+(users)+"the same value in the array: "+(flag)+"it value repeated: "+(counter));
     }
 }
