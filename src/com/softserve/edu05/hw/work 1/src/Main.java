275f02defa74a011c94eb4b493265ede90a45875
Mandarinka4 <maryna.osypova.oi.2022@lpnu.ua>
2023-08-07 19:04:17 +0300

    hw #5

---
+++ src/com/softserve/edu05/hw/work 1/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Enter a month number (1-12): ");
+        int index = SCANNER.nextInt();
+
+        int[] daysInMonth = {0, 31, 28, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        int Days = daysInMonth [index];
+        System.out.println(Days + " days in the " + index + " month");
+        }
+    }
