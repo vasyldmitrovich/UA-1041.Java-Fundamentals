50cf7d1a2314fb1fffedac0897ddbda886377664
alyonaMol <alen4hukm@gmail.com>
2023-08-08 16:13:03 +0300

    Practic1

---
+++ src/com/softserve/edu04/Practical/task2/Days.java
@@ -0,0 +1,23 @@
+package com.softserve.edu04.Practical.task2;
+
+public enum Days {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+
+
+
+
+
+}
+
+
+
+
+
+
+
