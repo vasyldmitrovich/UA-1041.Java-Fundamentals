7b9aba958054d5c4e611b27e5f8232fecd6d38a6
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-08-08 14:25:09 +0300

    homework task 2  less5

--- src/com/softserve/edu05/hw/task_2/SumNumbers.java
+++ src/com/softserve/edu05/hw/task_2/SumNumbers.java
@@ -4,23 +4,23 @@ import java.util.Scanner;
 
 public class SumNumbers {
     public static void main(String[] args) {
-        boolean reaped = true;
+        boolean reaped;
         do {
 
             int first = getNumber("enter first number:");
             int second = getNumber("enter second number:");
             System.out.println("The sum of numbers:" + (first + second));
             System.out.println("----------------------------");
-            reaped = doReaped("do you want to perform the operation again?(yes/no)");
+            reaped = doReaped();
         }
         while (reaped);
 
     }
 
-    private static boolean doReaped(String text) {
+    private static boolean doReaped() {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println(text);
+        System.out.println("do you want to perform the operation again?(yes/no)");
         String answer = sc.nextLine();
         return answer.equalsIgnoreCase("yes");
     }
