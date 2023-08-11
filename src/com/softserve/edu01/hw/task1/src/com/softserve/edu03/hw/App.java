9a87b24dd94bb12b1071722123a663adf24006dd
Anastasia Rusanova <a.i.rusanova@i.ua>
2023-08-11 11:54:14 +0300

    edu03

---
+++ src/com/softserve/edu01/hw/task1/src/com/softserve/edu03/hw/App.java
@@ -0,0 +1,10 @@
+package com.softserve.edu03.hw;
+
+public class App {
+    public static void main(String[] args) {
+        // тут буду викликати мої методи
+
+        Task1.myMethod();
+
+    }
+}
