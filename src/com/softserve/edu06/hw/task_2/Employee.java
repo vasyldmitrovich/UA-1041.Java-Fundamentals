5c185dad148ef9b923dc6cff5d89be5ed56e762b
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-08-12 20:57:03 +0300

    homework task2  less6

---
+++ src/com/softserve/edu06/hw/task_2/Employee.java
@@ -0,0 +1,81 @@
+package com.softserve.edu06.hw.task_2;
+
+import java.util.Objects;
+
+public class Employee {
+    private String name;
+    private int age;
+    private double salary;
+
+    public Employee() {
+    }
+
+    public Employee(String name, int age, double salary) {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Employee employee = (Employee) o;
+
+        if (age != employee.age) return false;
+        if (Double.compare(employee.salary, salary) != 0) return false;
+        return Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        temp = Double.doubleToLongBits(salary);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
+    }
+
+    public String report() {
+        return String.format(
+                "Name: %s, Age: %d years, Salary: \u20B4 %.2f.", name, age, salary
+        );
+
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", salary=" + salary +
+                '}';
+    }
+}
---
+++ src/com/softserve/edu06/hw/task_2/Main.java
@@ -0,0 +1,15 @@
+package com.softserve.edu06.hw.task_2;
+
+public class Main {
+    public static void main(String[] args) {
+        Developer developer1 = new Developer("Taras", 32, 32735.35, "Average Java developer");
+        Employee employee1 = new Employee("Igor", 46, 16000.50);
+
+
+        System.out.println(developer1);
+        System.out.println(developer1.report());
+        System.out.println("----------------------");
+        System.out.println(employee1);
+        System.out.println(employee1.report());
+    }
+}
