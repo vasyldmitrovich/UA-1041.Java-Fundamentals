daee6a372b13ba740f61c452159c36bbd9ae9255
annakachmarchyk <116542188+annakachmarchyk@users.noreply.github.com>
2023-08-14 16:24:07 +0300

    Fixed all mistakes

---
+++ src/com/softserve/edu08/hm/MainPerson.java
@@ -0,0 +1,19 @@
+package com.softserve.edu08.hm;
+
+class MainPerson {
+    public static void main(String[] args) {
+        Student student1 = new Student(new Person.FullName("Mary", "Smith"), 25, 2);
+        Student student2 = new Student(new Person.FullName("John", "Collins"), 30, 3);
+        System.out.println(student1.info());
+        System.out.println(student1.activity());
+        System.out.println(student2.info());
+        System.out.println(student2.activity());
+
+        Student student3 = (Student) student1.clone();
+        System.out.println(student3.info());
+        student3.course = 4;
+        System.out.println(student3.info());
+
+    }
+}
+
---
+++ src/com/softserve/edu08/hm/Student.java
@@ -0,0 +1,30 @@
+package com.softserve.edu08.hm;
+
+class Student extends Person implements Cloneable {
+    int course;
+
+    public Student(FullName fullName, int age, int course) {
+        super(fullName, age);
+        this.course = course;
+    }
+
+    @Override
+    public String activity() {
+        return "I study at university";
+    }
+
+    @Override
+    String info() {
+        return super.info() + "\ncourse: " + course;
+    }
+
+    @Override
+    protected Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
