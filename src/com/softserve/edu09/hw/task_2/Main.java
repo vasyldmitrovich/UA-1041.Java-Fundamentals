742dd9457cc387ee322b9dd73ce6a661b3037a12
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-08-19 23:13:05 +0300

    homework tasks2 less9

---
+++ src/com/softserve/edu09/hw/task_2/Main.java
@@ -0,0 +1,39 @@
+package com.softserve.edu09.hw.task_2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        List<Student> students = new ArrayList<>();
+        addStudentsToList(students);
+        for (var el : students
+        ) {
+            System.out.println(el);
+        }
+
+        System.out.println("_______________________________");
+        Student.removeToAverageGrade(students);
+
+        System.out.println("_______________________________");
+        int courseNumber = getCourseNumber();
+        Student.printStudents(students, courseNumber);
+    }
+
+    private static int getCourseNumber() {
+        System.out.println("Enter course number:");
+        return SCANNER.nextInt();
+    }
+
+    private static void addStudentsToList(List<Student> students) {
+        students.add(new Student("John", 1, 85, 4.5, 4.1, 3.7));
+        students.add(new Student("Silvia", 3, 90, 3.5, 2.1, 2.7));
+        students.add(new Student("Orest", 2, 85, 3.1, 2.5, 4));
+        students.add(new Student("Makar", 8, 95, 5, 2, 3));
+        students.add(new Student("David", 5, 90, 2, 2, 3));
+    }
+
+}
