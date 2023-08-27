a65edb0ec086700a9dc63109d90583955fc608d1
Holotsvan Ivan <iholotsvan@gmail.com>
2023-08-27 10:44:38 +0300

    edu10 hw

---
+++ src/com/softserve/edu10/hw/Main.java
@@ -0,0 +1,12 @@
+package com.softserve.edu10.hw;
+
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public static Set<Integer> union(Set<Integer> set1, Set<Integer> set2) {
+        return null;
+    }
+}
