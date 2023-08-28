452516f6f85ddd833bc11fb83da3e65a5b5e09be
Mandarinka4 <maryna.osypova.oi.2022@lpnu.ua>
2023-08-28 14:40:52 +0300

    pt #11

---
+++ src/com/softserve/edu11/pt/work 1/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String text = "SoftServe Academy";
+        String fragment = "SoftServe";
+        System.out.println(text.contains(fragment));
+    }
+}
