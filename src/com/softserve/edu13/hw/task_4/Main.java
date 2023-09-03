d63dcef41b7e13a63e08a9fb5fd2a5dc4f053c79
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-09-03 15:34:40 +0300

    hw tasks4 less13

---
+++ src/com/softserve/edu13/hw/task_4/Main.java
@@ -0,0 +1,27 @@
+package com.softserve.edu13.hw.task_4;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.TemporalAccessor;
+
+public class Main {
+    public static void main(String[] args) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate birthDate = LocalDate.from(formatter.parse("03.04.1990"));
+        int month = 0;
+        System.out.println("day of week in birth year was: " + dayOfWeekAfterSomeMonths(birthDate, month));
+        month = 6;
+        System.out.printf("day of week after %d months after birth day was:%s%n", month, dayOfWeekAfterSomeMonths(birthDate, month));
+        month = 12;
+        System.out.printf("day of week after %d months after birth day was:%s%n", month, dayOfWeekAfterSomeMonths(birthDate, month));
+    }
+
+
+    private static DayOfWeek dayOfWeekAfterSomeMonths(LocalDate birthDate, int month) {
+
+        return birthDate.plusMonths(month).getDayOfWeek();
+    }
+
+
+}
