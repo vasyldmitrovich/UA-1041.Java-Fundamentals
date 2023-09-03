3b7d7ef1cd8f2be0701394d547153965960a2b7a
Andrii Synytsia <andreysinitsa4315@gmail.com>
2023-09-03 21:06:29 +0200

    edu13 | Practical tasks

---
+++ src/com/softserve/edu13/practicaltasks/Main.java
@@ -0,0 +1,65 @@
+package com.softserve.edu13.practicaltasks;
+
+import java.time.*;
+import java.util.*;
+import java.time.temporal.TemporalAdjusters;
+import java.util.function.IntPredicate;
+
+public class Main {
+    public static void main(String[] args) {
+
+//       Show which today is a day of a week
+        getLocalDayOfWeek();
+
+//        Show a date of first Monday for current month
+        firstMondayOfCurrentMonth();
+
+//        Write a method for sorting list of Strings
+        var names = promptUserList();
+        System.out.println("List before sorting: " + names);
+        var sortedList = sort(names);
+        System.out.println("List after sorting: " + sortedList);
+
+//        Method userCounter() return count of elements in array that satisfy the condition defined by the second argument
+        userCounter();
+    }
+
+    private static void userCounter() {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8};
+        System.out.println(count(nums, i -> i < 6));
+        System.out.println(count(nums, i -> i % 2 != 0));
+        System.out.println(count(nums, i -> i >= 3 && i <= 6));
+    }
+
+    private static void firstMondayOfCurrentMonth() {
+        LocalDate date = LocalDate.now();
+        var firstMonday = date.with(TemporalAdjusters.firstInMonth(DayOfWeek.MONDAY));
+        System.out.println("Date of first Monday of the month is: " + firstMonday);
+    }
+
+    private static void getLocalDayOfWeek() {
+        LocalDate date = LocalDate.now();
+        System.out.println("Day of the week: " + LocalDate.now().getDayOfWeek());
+    }
+
+    public static int count(int[] numbers, IntPredicate condition) {
+        int counter = 0;
+        for (int i : numbers) {
+            if (condition.test(i)) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    public static List promptUserList() {
+        List list = new ArrayList<>();
+        list.addAll(Arrays.asList("Bob", "Jane", "Alice", "Matt", "Robert", "Dan", "Mark", "Melony", "Lora"));
+        return list;
+    }
+
+    public static List sort(List<String> list) {
+        list.sort(Comparator.naturalOrder());
+        return list;
+    }
+}
