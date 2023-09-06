b288426814268cfb2d1a3496383d2597b3f8ced6
annakachmarchyk <116542188+annakachmarchyk@users.noreply.github.com>
2023-09-07 00:44:31 +0300

    Finished edu14

---
+++ src/com/softserve/edu14/pr/Task2.java
@@ -0,0 +1,18 @@
+package com.softserve.edu14.pr;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Task2 {
+    public static void main(String[] args) {
+        List<Integer> primes = Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        var stats = primes.stream()
+                .mapToInt(i -> i)
+                .summaryStatistics();
+        System.out.println("min = "+ stats.getMin());
+        System.out.println("max = "+ stats.getMax());
+        System.out.println("count = "+ stats.getCount());
+        System.out.println("sum = "+ stats.getSum());
+        System.out.println("average = "+ stats.getAverage());
+    }
+}
