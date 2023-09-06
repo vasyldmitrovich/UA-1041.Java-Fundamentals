1eb390c9f140d63f7b02ec786a865e4a6d4a3219
Vasylyshyn Dmytro <dmitrovasilisin24@gmail.com>
2023-09-06 20:35:17 +0300

    First commit

--- src/com/softserve/edu14/pt/Task2/Employee.java
+++ src/com/softserve/edu14/pt/Task2/Employee.java
@@ -1,13 +1,6 @@
 package com.softserve.edu14.pt.Task2;
 
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.stream.Stream;
 
-import static java.util.stream.Collectors.counting;
-import static java.util.stream.Collectors.groupingBy;
 
 public class Employee {
     private int id;
