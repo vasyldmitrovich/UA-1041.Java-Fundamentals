2038107b5eb76f40dad7a1027ad0c7e747905b6a
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-09-06 13:40:47 +0300

    hw tasks1 less15

---
+++ src/com/softserve/edu15/hw/task_1/Main.java
@@ -0,0 +1,53 @@
+package com.softserve.edu15.hw.task_1;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+        var t1 = getThread1();
+        var t2 = getThread2();
+        var t3 = getThread3();
+        t1.start();
+        t2.start();
+        t3.start();
+        t1.join();
+        t2.join();
+    }
+
+    private static Thread getThread1() {
+        return new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("I");
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+
+    private static Thread getThread2() {
+        return new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("like");
+                try {
+                    Thread.sleep(3000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+    private static Thread getThread3() {
+        return new Thread(() -> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Java");
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+    }
+}
