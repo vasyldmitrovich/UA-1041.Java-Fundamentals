a501670c29c2b8b12f6730f8e55dc030ca2e0782
Nikolay Vergeles <Vergeles4Nikolay@gmail.com>
2023-09-09 19:47:01 +0300

    hw tasks1 less16.1

--- src/com/softserve/edu16_IO/hw/task_1/Main.java
+++ src/com/softserve/edu16_IO/hw/task_1/Main.java
@@ -4,6 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 import java.util.Comparator;
 import java.util.List;
 
@@ -18,20 +19,16 @@ public class Main {
 
     private static void writeToOtherFile(List<String> text, Path file2) throws IOException {
 //  1) number of lines in file1.txt
-        StringBuilder data = new StringBuilder("text contains:" + text.size() + "lines\n");
+        StringBuilder data = new StringBuilder("text contains: " + text.size() + " lines\n");
 
 //  2) the longest line in file1.txt
         var longLine = text.stream()
                 .max(Comparator.comparing(String::length))
                 .get();
-        data.append("the longest line:" + longLine + "\n");
+        data.append("the longest line: " + longLine + "\n");
 
 //  3) your name and birthday date
         data.append("My name is Mykola,\nMy birthday 03.04.90");
-        try (var fw = Files.newBufferedWriter(file2)) {
-            fw.write(data.toString());
-            System.out.println("data was writing to " + file2.toString());
-        }
-
+        Files.writeString(file2, data);
     }
 }
