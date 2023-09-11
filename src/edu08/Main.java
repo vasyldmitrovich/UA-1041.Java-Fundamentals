b10704994f5a607518398c4a61c04403f87b51b9
OLha Derbak <olhaderbak@gmail.com>
2023-09-11 16:47:15 +0300

    Tasks from 8th lecture

---
+++ src/edu08/Main.java
@@ -0,0 +1,24 @@
+package edu08;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student(new FullName("Olha", "Derbak"), 31, 92);
+        Student student2 = new Student(new FullName("Ivan", "Ivanov"), 25, 97);
+        Student student3 = student1.clone();
+        student3.setCourseNumber(101);
+
+        System.out.println("Student1:");
+        System.out.println(student1.info());
+        System.out.println(student1.activity());
+
+        System.out.println();
+        System.out.println("Student2:");
+        System.out.println(student2.info());
+        System.out.println(student2.activity());
+
+        System.out.println();
+        System.out.println("Student3:");
+        System.out.println(student3.info());
+        System.out.println(student3.activity());
+    }
+}
---
+++ src/edu08/Student.java
@@ -0,0 +1,43 @@
+package edu08;
+
+public class Student extends Person implements Cloneable {
+    private int courseNumber;
+    private Student fullName;
+
+    public Student() {
+    }
+
+    public Student(FullName fullName, int age, int courseNumber) {
+        super(fullName, age);
+        this.courseNumber = courseNumber;
+    }
+
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(int courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    @Override
+    public String info() {
+        return "the course the student:" + this.courseNumber + ", " + super.info();
+    }
+
+    @Override
+    public String activity() {
+        return "I study at university the course:" + this.courseNumber;
+    }
+
+    @Override
+    public Student clone() {
+        try {
+            var res = (Student) super.clone();
+            res.fullName = fullName.clone();
+            return res;
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
