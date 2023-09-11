83ca6dd581c4b93891aac137981121570cc59754
OLha Derbak <olhaderbak@gmail.com>
2023-09-11 18:38:15 +0300

    Tasks from 12th lecture

---
+++ src/edu12/Task1.java
@@ -0,0 +1,34 @@
+package edu12;
+import java.util.Scanner;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static final Scanner SCANNER = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (true) {
+            try {
+                System.out.println("Enter first double number(d1):");
+                double d1 = Double.parseDouble(SCANNER.nextLine());
+                System.out.println("Enter second double number(d2):");
+                double d2 = Double.parseDouble(SCANNER.nextLine());
+                System.out.println("dividing d1/d2:" + div(d1, d2));
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("input must be double number, try again!");
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
+    public static double div(double d1, double d2) {
+
+        if (d2 == 0.0) {
+            throw new IllegalArgumentException("second number can`t be 0, try again!");
+        }
+        return d1 / d2;
+    }
+}
