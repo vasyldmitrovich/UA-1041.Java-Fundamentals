e8518500ef9d1bbf06172cc8dd891b2c8df73f64
Yezikov Maksym <maksimezikov@gmail.com>
2023-09-18 21:38:10 +0300

    Homework 15

--- src/com/softserve/edu15/hw/Hw02.java
+++ src/com/softserve/edu15/hw/Hw02.java
@@ -1,49 +1,15 @@
 package com.softserve.edu15.hw;
 
-class Hw02 extends Thread {
-    private final static Object LOCK1 = new Object();
-    private final static Object LOCK2 = new Object();
-
-//    now we have deadlock
-    static Thread t1 = new Thread(() -> {
-        synchronized (LOCK1) {
-            System.out.println("Start Thread 1");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-
-            synchronized (LOCK2) {
-                System.out.println("End Thread 1");
-            }
+class Hw02 {
+    public static synchronized void main(String[] args) {
+        var tr = new Thread(() -> main(new String[]{}));
+        tr.start();
+        System.out.println("Start");
+        System.out.println(tr.getState());
+        try {
+            tr.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-    });
-    static Thread t2 = new Thread(() -> {
-        // for the end of the method main() need change monitor: synchronized (LOCK1)
-        synchronized (LOCK2) {
-            System.out.println("Start Thread 2");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            // for the end of the method main() need change monitor: synchronized (LOCK2)
-            synchronized (LOCK1) {
-                System.out.println("End Thread 2");
-            }
-        }
-    });
-
-    public static void main(String[] args) throws InterruptedException {
-
-        t1.start();
-        t2.start();
-
-        Thread.sleep(20);
-
-        System.out.println(t1.getState() + " Thread 1");
-        System.out.println(t2.getState() + " Thread 2");
-
     }
 }
